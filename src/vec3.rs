c6c7ca2c01dd30ac7a2a8491e7f4562ffad767be
Thien Tran <gau.nernst@yahoo.com.sg>
2023-03-06 00:25:13 +0800

    use macros to generate operator overloading

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,5 @@
 use rand::prelude::*;
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vec3 {
@@ -18,30 +18,30 @@ impl Vec3 {
     pub fn one() -> Vec3 {
         Vec3::new(1.0, 1.0, 1.0)
     }
-    pub fn length(self) -> f64 {
+    pub fn length(&self) -> f64 {
         self.length2().sqrt()
     }
-    pub fn length2(self) -> f64 {
+    pub fn length2(&self) -> f64 {
         self.dot(self)
     }
-    pub fn dot(self, other: Vec3) -> f64 {
+    pub fn dot(&self, other: &Vec3) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
-    pub fn cross(self, other: Vec3) -> Vec3 {
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
         Vec3::new(
             self.y * other.z - other.y * self.z,
             self.z * other.x - other.z * self.x,
             self.x * other.y - other.x * self.y,
         )
     }
-    pub fn normalize(self) -> Vec3 {
+    pub fn normalize(&self) -> Vec3 {
         self / self.length()
     }
-    pub fn reflect(self, n: Vec3) -> Vec3 {
+    pub fn reflect(&self, n: &Vec3) -> Vec3 {
         self - 2.0 * self.dot(n) * n
     }
-    pub fn refract(self, n: Vec3, eta: f64) -> Vec3 {
-        let cos_theta = f64::min(-self.dot(n), 1.0);
+    pub fn refract(&self, n: &Vec3, eta: f64) -> Vec3 {
+        let cos_theta = 1f64.min(-self.dot(n));
         let r_out_perp = eta * (self + cos_theta * n);
         let r_out_para = -(1.0 - r_out_perp.length2()).abs().sqrt() * n;
         r_out_perp + r_out_para
@@ -84,75 +84,53 @@ impl Neg for Vec3 {
     }
 }
 
-impl Add<Vec3> for Vec3 {
-    type Output = Vec3;
-    fn add(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
-    }
+macro_rules! impl_op_f64 {
+    ($t:ty, $tr:ident, $method:ident) => {
+        impl $tr<f64> for $t {
+            type Output = Vec3;
+            fn $method(self, rhs: f64) -> Vec3 {
+                Vec3::new(
+                    self.x.$method(rhs),
+                    self.y.$method(rhs),
+                    self.z.$method(rhs),
+                )
+            }
+        }
+        impl $tr<$t> for f64 {
+            type Output = Vec3;
+            fn $method(self, rhs: $t) -> Vec3 {
+                Vec3::new(
+                    self.$method(rhs.x),
+                    self.$method(rhs.y),
+                    self.$method(rhs.z),
+                )
+            }
+        }
+    };
 }
-
-impl Add<f64> for Vec3 {
-    type Output = Vec3;
-    fn add(self, rhs: f64) -> Vec3 {
-        Vec3::new(self.x + rhs, self.y + rhs, self.z + rhs)
-    }
+macro_rules! impl_op {
+    ($t:ty, $tr:ident, $method:ident) => {
+        impl_op!($t, $t, $tr, $method);
+        impl_op!($t, &$t, $tr, $method);
+        impl_op!(&$t, $t, $tr, $method);
+        impl_op!(&$t, &$t, $tr, $method);
+        impl_op_f64!($t, $tr, $method);
+        impl_op_f64!(&$t, $tr, $method);
+    };
+    ($t1:ty, $t2:ty, $tr:ident, $method:ident) => {
+        impl $tr<$t2> for $t1 {
+            type Output = Vec3;
+            fn $method(self, rhs: $t2) -> Vec3 {
+                Vec3::new(
+                    self.x.$method(rhs.x),
+                    self.y.$method(rhs.y),
+                    self.z.$method(rhs.z),
+                )
+            }
+        }
+    };
 }
-
-impl AddAssign<Vec3> for Vec3 {
-    fn add_assign(&mut self, rhs: Vec3) {
-        *self = Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
-    }
-}
-
-impl Sub<Vec3> for Vec3 {
-    type Output = Vec3;
-    fn sub(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
-    }
-}
-
-impl SubAssign<Vec3> for Vec3 {
-    fn sub_assign(&mut self, rhs: Vec3) {
-        *self = Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
-    }
-}
-
-impl Mul<Vec3> for Vec3 {
-    type Output = Vec3;
-    fn mul(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
-    }
-}
-
-impl Mul<f64> for Vec3 {
-    type Output = Vec3;
-    fn mul(self, rhs: f64) -> Vec3 {
-        Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
-    }
-}
-
-impl Mul<Vec3> for f64 {
-    type Output = Vec3;
-    fn mul(self, rhs: Vec3) -> Vec3 {
-        rhs * self
-    }
-}
-
-impl MulAssign<f64> for Vec3 {
-    fn mul_assign(&mut self, rhs: f64) {
-        *self = Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
-    }
-}
-
-impl Div<f64> for Vec3 {
-    type Output = Vec3;
-    fn div(self, rhs: f64) -> Vec3 {
-        self * (1.0 / rhs)
-    }
-}
-
-impl DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, rhs: f64) {
-        *self *= 1.0 / rhs
-    }
-}
+impl_op!(Vec3, Add, add);
+impl_op!(Vec3, Sub, sub);
+impl_op!(Vec3, Mul, mul);
+impl_op!(Vec3, Div, div);
